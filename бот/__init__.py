a4a939caaa28ea06cb7f348589f2e7fd280c938e
KioFox <93644625+KioFox@users.noreply.github.com>
2021-11-08 00:09:58 +0500

    Update and rename bot/__init__.py to бот/__init__.py

--- бот/__init__.py
+++ бот/__init__.py
@@ -11,15 +11,15 @@ from logging.handlers import RotatingFileHandler
 from .translation import Translation
 
 # Change Accordingly While Deploying To A VPS
-APP_ID = int(os.environ.get("APP_ID"))
+APP_ID = 9101527
 
-API_HASH = os.environ.get("API_HASH")
+API_HASH = 3d860e0047f2098289ec4eca8649b7da
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = 2095742657:AAHhESyW6ic8FpVr4lJ7GsbhkR9sXsw3X2g
 
-DB_URI = os.environ.get("DB_URI")
+DB_URI = https://github.com/TeamUltroid/Ultroid
 
-USER_SESSION = os.environ.get("USER_SESSION")
+USER_SESSION = 1ApWapzMBu26jYooDIGc_pul7j3hpgRdO_HQHDpPwx5Ff_qERAd4kceCS7ikYbN2ijI4ZbuvNALwEV8wCW6Zfi7BbF3G0nZySA4f2S6lnvCp4v-yvmqJw1k3s3mpNwy466Te5o3YDOfjrYESaLhAwpeDj_C6kp9Gr0PH6uZHeL5COWfZd8stfVfT9nGGMLeNAjLz2rdLtRnJle1n-7eIyci-LkF07ZjrulO-oj2Zjcjf7nlUtbd81QGIzrmeHyPzv4vSxf08en9kACToFLbpi_aJ5DM1s15hnTi7BDJuQPzxOhZ1khWtuFwHj6NC1Z59yqKwj5FVfwQ5DYOmF-KxIZsxsLKakkBw=
 
 VERIFY = {}
 
